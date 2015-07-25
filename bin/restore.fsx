097fdcd9012df751ecd31e12fa2b074ec75bb71d
Alex Swan <metal10k@hotmail.com>
2015-07-25 17:52:00 +0100

    Added restore functionality

---
+++ bin/restore.fsx
@@ -0,0 +1,29 @@
+#load "..\lib\Resolver.fsx"
+#load "..\lib\Process.fsx"
+
+open System.IO
+
+type FolderType = 
+    | Paket of string
+    | Bower of string
+
+let rec getFolders path =
+    seq{
+        for filePath in Directory.EnumerateFiles(path) do
+            match Path.GetFileName(filePath) with
+            | "paket.dependencies" -> yield Paket(path)
+            | "bower.json" -> yield Bower(path)
+            | _ -> ()
+
+        for d in Directory.EnumerateDirectories(path) do
+            yield! getFolders d
+        }
+
+Process.shellExecute("npm install")  |> Process.print
+
+let dirName = System.Environment.CurrentDirectory
+for f in getFolders dirName do
+    match f with
+    | Paket(s) -> Process.shellExecute("cd " + s + "&& paket install") |> Process.print
+    | Bower(s) -> Process.shellExecute("cd " + s + "&& bower install") |> Process.print
+    
