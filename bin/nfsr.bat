69f462244a29b2eb6af72eb856680e5ea72cc5da
Alex Swan <metal10k@hotmail.com>
2015-07-25 10:34:32 +0100

    Added copyglobal helper script

--- bin/nfsr.bat
+++ bin/nfsr.bat
@@ -10,4 +10,4 @@ IF EXIST "%ProgramFiles(x86)%\Microsoft SDKs\F#\3.0\Framework\v4.0\" (
 	set fsharppath="%ProgramFiles(x86)%\Microsoft SDKs\F#\3.0\Framework\v4.0"
 )
 
-%fsharppath%\Fsi.exe %~dp0\nfsr.fsx %*
+%fsharppath%\Fsi.exe %~dp0\nfsr.fsx %* --debug+
