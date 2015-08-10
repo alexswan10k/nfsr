23dcded44ddd0a80cfc81e5664badcb716840f37
Alex Swan <metal10k@hotmail.com>
2015-08-10 20:28:38 +0100

    Restore now refreshes dynamic refs. Added helper text files.

--- bin/restore.fsx
+++ bin/restore.fsx
@@ -6,6 +6,7 @@ open System.IO
 type FolderType = 
     | Paket of string
     | Bower of string
+    | DynamicRefs of string
 
 let rec getFolders path =
     seq{
@@ -13,6 +14,7 @@ let rec getFolders path =
             match Path.GetFileName(filePath) with
             | "paket.dependencies" -> yield Paket(path)
             | "bower.json" -> yield Bower(path)
+            | "_DynamicReferences.lock" -> yield DynamicRefs(path)
             | _ -> ()
 
         for d in Directory.EnumerateDirectories(path) do
@@ -26,5 +28,5 @@ for f in getFolders dirName do
     match f with
     | Paket(s) -> Process.shellExecute("cd " + s + "&& paket install") |> Process.print
     | Bower(s) -> Process.shellExecute("cd " + s + "&& bower install") |> Process.print
-    
+    | DynamicRefs(s) -> Process.shellExecute("cd " + s + "&& nfsr dynamic-refs refresh") |> Process.print
 //refresh dynamic refs
