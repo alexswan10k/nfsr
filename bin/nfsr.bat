572d7dd9f6747cb35b458706602ef42d8100025b
Alex Swan <Alexander_Swan@fundslibrary.co.uk>
2016-08-04 16:45:08 +0100

    Minor reorder to batch file to fix issue where legacy folders were taking prescedence over newer ones.

--- bin/nfsr.bat
+++ bin/nfsr.bat
@@ -7,14 +7,14 @@ IF EXIST %nfsrExePath% (
 )
 
 IF NOT EXIST %nfsrExePath% (
-	IF EXIST "%ProgramFiles(x86)%\Microsoft SDKs\F#\4.0\Framework\v4.0\" (
-		set fsharppath="%ProgramFiles(x86)%\Microsoft SDKs\F#\4.0\Framework\v4.0"
+	IF EXIST "%ProgramFiles(x86)%\Microsoft SDKs\F#\3.0\Framework\v4.0\" (
+		set fsharppath="%ProgramFiles(x86)%\Microsoft SDKs\F#\3.0\Framework\v4.0"
 	)
 	IF EXIST "%ProgramFiles(x86)%\Microsoft SDKs\F#\3.1\Framework\v4.0\" (
 		set fsharppath="%ProgramFiles(x86)%\Microsoft SDKs\F#\3.1\Framework\v4.0"
 	)
-	IF EXIST "%ProgramFiles(x86)%\Microsoft SDKs\F#\3.0\Framework\v4.0\" (
-		set fsharppath="%ProgramFiles(x86)%\Microsoft SDKs\F#\3.0\Framework\v4.0"
+	IF EXIST "%ProgramFiles(x86)%\Microsoft SDKs\F#\4.0\Framework\v4.0\" (
+		set fsharppath="%ProgramFiles(x86)%\Microsoft SDKs\F#\4.0\Framework\v4.0"
 	)
 
 	%fsharppath%\Fsi.exe %~dp0\nfsr.fsx %* 
